02547d676b9d2d701a3ae427d16660f01ef012d6
dilana <dilana@dilana.in>
2020-04-12 21:30:54 +0300

    added flatlist dots

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -13,6 +13,7 @@ interface MyProps {
 interface MyState {
     locations: Array<string>,
     isLoading: boolean,
+    activeLocation: number,
     weather: Array<WeatherInterface>,
     error: string,
 }
@@ -20,6 +21,7 @@ interface MyState {
 class Main extends Component<MyProps & NavigationInjectedProps, MyState> {
     state: MyState = {
         isLoading: true,
+        activeLocation: 0,
         locations: [],
         weather: undefined,
         error: undefined,
@@ -35,6 +37,25 @@ class Main extends Component<MyProps & NavigationInjectedProps, MyState> {
             alignItems: 'center',
             justifyContent: 'center',
             backgroundColor: '#000',
+        },
+        dotsContainer: {
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            bottom: 25,
+            flex: 0,
+            height: 10,
+            width: '100%',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        dot: {
+            width: 10,
+            height: 10,
+            borderRadius: 10 / 2,
+            backgroundColor: 'rgba(255, 255, 255, 0.3)',
+            marginHorizontal: 2.5,
         },
         flatlist: {
             width: Dimensions.get('window').width,
@@ -72,31 +93,45 @@ class Main extends Component<MyProps & NavigationInjectedProps, MyState> {
                         {isLoading ? (
                             <View style={this.styles.loadingContainer}/>
                         ) : (
-                            <FlatList
-                                horizontal
-                                pagingEnabled
-                                disableIntervalMomentum
-                                data={weather}
-                                initialNumToRender={5}
-                                windowSize={5}
-                                viewabilityConfig={
-                                    {
-                                        itemVisiblePercentThreshold: 100,
+                            <View style={this.styles.container}>
+                                <FlatList
+                                    horizontal
+                                    pagingEnabled
+                                    disableIntervalMomentum
+                                    data={weather}
+                                    initialNumToRender={5}
+                                    windowSize={5}
+                                    viewabilityConfig={
+                                        {
+                                            itemVisiblePercentThreshold: 100,
+                                        }
                                     }
-                                }
-                                style={this.styles.flatlist}
-                                keyExtractor={weather => weather.query}
-                                showsHorizontalScrollIndicator={true}
-                                renderItem={({item, index}) => (
-                                    <Weather weather={item}/>
-                                )}
-                                onViewableItemsChanged={this.onViewableItemsChanged}
-                            />
+                                    style={this.styles.flatlist}
+                                    keyExtractor={weather => weather.query}
+                                    showsHorizontalScrollIndicator={false}
+                                    renderItem={({item, index}) => (
+                                        <Weather weather={item}/>
+                                    )}
+                                    onViewableItemsChanged={this.onViewableItemsChanged}
+                                    onScroll={this.handleScroll}
+                                />
+                                <View style={this.styles.dotsContainer}>
+                                    {Array.from({length: weather.length}, (v, k) => k).map((prop: any, key) => {
+                                        return (
+                                            <View key={key} style={[this.styles.dot, {backgroundColor: this.state.activeLocation === key ? 'rgba(255, 255, 255, 1)' : 'rgba(255, 255, 255, 0.3)'}]}/>
+                                        );
+                                    })}
+                                </View>
+                            </View>
                         )}
                     </View>
                 )}
             </View>
         );
+    }
+
+    private handleScroll(event: Object) {
+        console.log(event);
     }
 
     private onViewableItemsChanged({viewableItems, changed}) {
@@ -107,6 +142,8 @@ class Main extends Component<MyProps & NavigationInjectedProps, MyState> {
         if (viewableItems.length > 0) {
             const index = viewableItems[0].index;
             const query = viewableItems[0].item.query;
+
+            this.setState({activeLocation: index});
 
             // @ts-ignore
             this.props.navigation.setOptions({title: viewableItems[0].item.city});
