6096c55003f9e35c70e4e2b3ee82f59ac05ced05
dilana <dilana@dilana.in>
2020-04-14 16:17:52 +0300

    moved declarations to proper file

--- declarations.d.ts
+++ declarations.d.ts
@@ -3,3 +3,41 @@ declare module "*.svg" {
     const content: React.FC<SvgProps>;
     export default content;
 }
+
+declare interface SettingsInterface {
+    temperature: boolean
+    speed: boolean
+    pressure: boolean
+    time: boolean
+}
+
+declare interface WeatherInterface {
+    query: string,
+    updatedAt: any,
+    temperature: number,
+    feelsLike: number,
+    humidity: number,
+    pressure: number,
+    tempMax: number,
+    tempMin: number,
+    sunrise: number,
+    sunset: number,
+    city: string,
+    country: string,
+    weatherCondition: {
+        description: string,
+        icon: string,
+        id: number,
+        main: string,
+    },
+    wind: {
+        speed: number,
+        deg: number
+    },
+    forecast?: Array<{
+        code: number,
+        day: string,
+        tempMax: number,
+        tempMin: number,
+    }>
+}
--- types/settings.d.ts
+++
@@ -1,6 +0,0 @@
-declare interface SettingsInterface {
-    temperature: boolean
-    speed: boolean
-    pressure: boolean
-    time: boolean
-}
